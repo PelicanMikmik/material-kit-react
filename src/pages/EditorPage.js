d216a2fc2ed88086eb05a5b101ef37d8919fc8fa
TZuri <47828500+PelicanMikmik@users.noreply.github.com>
2023-07-30 22:50:23 +0300

    user page editing

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -3,9 +3,6 @@ import { Helmet } from 'react-helmet-async';
 import { Container, Typography, Grid } from '@mui/material';
 
 // components
-import {
-  AppWidgetSummary,
-} from '../sections/@dashboard/app';
 
 import FrameEditor from "../components/Frames/Frame editor/FrameEditor";
 
@@ -27,26 +24,9 @@ export default function EditorPage(socket) {
         </Typography>
 
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={1} icon={'ant-design:android-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
-          </Grid>
-
           <Grid>
             <FrameEditor socket={socket} />
           </Grid>
-
         </Grid>
       </Container>
     </>
